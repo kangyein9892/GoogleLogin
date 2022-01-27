dc379fb6d25c1b819e37c0019f2ce3f24d9201d2
yein <inwon9892@naver.com>
2022-01-27 11:21:02 +0900

    구글 로그인

---
+++ app/src/main/java/com/android/googlelogin/LoginActivity.kt
@@ -0,0 +1,76 @@
+package com.android.googlelogin
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+
+class LoginActivity : AppCompatActivity() {
+
+    var googleSignClient : GoogleSignInClient? = null
+    val RC_SIGN_IN = 1000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        var googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        val xml_btn_main_googleLogin = findViewById<SignInButton>(R.id.google_sign_in_button)
+
+        xml_btn_main_googleLogin.setOnClickListener {
+            var signInIntent = googleSignInClient?.signInIntent
+            startActivityForResult(signInIntent, RC_SIGN_IN)
+        }
+    }
+    fun fun_MoveNextPage() {
+        var currentUser = FirebaseAuth.getInstance().currentUser
+        if(currentUser != null) {
+            startActivity(Intent(this, MainActivity::class.java))
+            this.finish()
+        }
+    }
+    fun fun_FirebaseAuthWithGoogle(acct : GoogleSignInAccount?) {
+        var credential = GoogleAuthProvider.getCredential(acct?.idToken, null)
+        FirebaseAuth.getInstance().signInWithCredential(credential).addOnCompleteListener {
+            task ->
+                if(task.isSuccessful){
+                    fun_MoveNextPage()
+                }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        fun_MoveNextPage()
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try{
+                val account = task.getResult(ApiException::class.java)
+                fun_FirebaseAuthWithGoogle(account)
+            }
+            catch(e: ApiException) {
+
+            }
+        }
+        else{
+
+        }
+    }
+}
---
+++ app/src/main/java/com/android/googlelogin/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.android.googlelogin
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+
+class MainActivity : AppCompatActivity() {
+
+    var auth: FirebaseAuth ?= null
+    private lateinit var googleSignInClient: GoogleSignInClient
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        auth = FirebaseAuth.getInstance()
+
+        val google_sign_out_button = findViewById<Button>(R.id.google_sign_out_button)
+
+        google_sign_out_button.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            googleSignInClient?.signOut()
+            var logoutIntent = Intent(this, LoginActivity::class.java)
+            logoutIntent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+            startActivity(logoutIntent)
+        }
+
+
+    }
+}
